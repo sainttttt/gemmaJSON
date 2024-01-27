aeb7baf730a772791707501389ff4a267e8a4d9d
Harrison Mbemba <hmbemba@gmail.com>
2024-01-27 00:11:02 -0500

    latest

--- src/gemmaJSON/compile.nim
+++ src/gemmaJSON/compile.nim
@@ -1,38 +1,119 @@
 import std/os
 import std/strformat
 import std/compilesettings
+import osproc
+import pathutils
+import options
 
-proc sh (cmd :string; dir :string= "") :void=
-  ## Executes the given shell command and writes the output to console.
-  ## Same as the nimscript version, but usable at compile time in static blocks.
-  ## Runs the command from `dir` when specified.
-  when defined(windows): {.warning: "running `sh -c` commands on Windows has not been tested".}
-  var command :string
-  # echo cmd
-  if dir != "":  command = &"cd {dir}; " & cmd
-  else:          command = cmd
-  discard gorgeEx(&"sh -c \"{$command}\"").output
+const cacheDir         = compilesettings.querySetting(nimcacheDir)
+const outDir           = compilesettings.querySetting(outDir)
+const curSrcPath       = currentSourcePath.parentDir
 
-const cacheDir  = compilesettings.querySetting(nimcacheDir)
-const outDir  = compilesettings.querySetting(outDir)
+echo "cacheDir: "      , cacheDir
+echo "outDir: "        , outDir
+echo "curSrcPath: "    , curSrcPath
 
-const curSrcPath = currentSourcePath.parentDir
+let gemmasimdjsonc_cpp = newStrictFile curSrcPath / "gemmasimdjsonc.cpp" 
+let simdjson_cpp       = newStrictFile curSrcPath / "simdjson.cpp"
 
-# {.passL: &"-L{trgDir}/ -lgemmaJSON".}
+let gemmasimdjsonc_o   = newStrictFile cacheDir / "gemmasimdjsonc.o" 
+let simdjson_o         = newStrictFile cacheDir / "simdjson.o"
 
-static:
-  sh(&"""mkdir -p {cacheDir}""")
-  when defined(linux):
-    sh(&"""g++ -fpic -c -O3 {curSrcPath}/gemmasimdjsonc.cpp {curSrcPath}/simdjson.cpp """, cacheDir)
+if not gemmasimdjsonc_cpp.ok:
+  echo fmt"gemmasimdjsonc.cpp not found! It should be in {curSrcPath}/gemmasimdjsonc.cpp"
+  quit(1)
+
+if not simdjson_cpp.ok:
+  echo fmt"simdjson.cpp not found in {curSrcPath}/simdjson.cpp"
+  quit(1)
+
+proc shell_execute(cmd :string) : tuple[output: string, exitCode: int] =
+  var full_command = ""
+
+  when defined(windows):
+    full_command = &"powershell -c {$cmd}"
+    return execCmdEx full_command
+  
+  elif defined(linux):
+    full_command = &"sh -c \"{$cmd}\" "
+    return execCmdEx full_command
+  
   elif defined(macosx):
-    sh(&"""clang++ -c -O3 {curSrcPath}/gemmasimdjsonc.cpp {curSrcPath}/simdjson.cpp -std=c++11""", cacheDir)
+    full_command = &"sh -c \"{$cmd}\" "
+    return execCmdEx full_command
 
-  # sh(&"""cp {cacheDir}/libgemmaJSON.so {outDir}/""")
+  else:
+    {.fatal: "OS not supported".}
+    echo "OS not supported"
+    quit(1)
+
+var 
+  output   : string
+  exitCode : int
+
+proc mk_cache_dir() =
+  # Create cache dir if it doesn't exist
+  when defined(windows):
+    (output, exitCode) = shell_execute(&"""New-Item -Path "{cacheDir}" -ItemType Directory -Force""")
+  else:
+    (output, exitCode) = shell_execute(&"""mkdir -p {cacheDir}""")
+
+  echo ""
+  echo "Creating cacheDir..."
+  echo "output: ", output
+  echo "exitCode: ", exitCode
+  if exitCode != 0:
+    echo fmt"Failed to create cacheDir at {cacheDir}"
+    echo ""
+    quit(1)
+  else:
+    echo fmt"Successfully created cacheDir at {cacheDir}"
+    echo ""
+
+proc compile_cpp() =
+  var check_if_gcc_installed = ""
+
+  # Compile C++ files
+  when defined(windows):
+    check_if_gcc_installed = "if ((Get-Command g++ -ErrorAction SilentlyContinue) -eq $null) { exit 1 }"
+    (output, exitCode)     = shell_execute(check_if_gcc_installed)
+    if exitCode != 0:
+      echo "g++ not found. Please install g++"
+      quit(1)
+
+    (output, exitCode) = shell_execute(&"""cd {cacheDir} ; g++ -fpic -c -O3 {$gemmasimdjsonc_cpp.val.get} {$simdjson_cpp.val.get} """)
+  elif defined(linux):
+    
+    #  This checks if g++ is in the path and redirects both standard output and standard error to /dev/null.
+    check_if_gcc_installed = "which g++ >/dev/null 2>&1"
+    (output, exitCode) = shell_execute(check_if_gcc_installed)
+    if exitCode != 0:
+      echo "g++ not found. Please install g++"
+      quit(1)
+
+    (output, exitCode) = shell_execute(&"""cd {cacheDir} ; g++ -fpic -c -O3 {$gemmasimdjsonc_cpp.val.get} {$simdjson_cpp.val.get} """)
+  elif defined(macosx):
+    (output, exitCode) = shell_execute(&"""cd {cacheDir} ; clang++ -c -O3 {$gemmasimdjsonc_cpp.val.get} {$simdjson_cpp.val.get} -std=c++11""")
+
+  echo "compiling C++ files..."
+  echo "output: "   , output
+  echo "exitCode: " , exitCode
+  if exitCode != 0:
+    echo "Failed to compile C++ files"
+    quit(1)
+  else:
+    echo "C++ files compiled"
+
+if not gemmasimdjsonc_o.ok or not simdjson_o.ok:
+  mk_cache_dir()  
+  compile_cpp()
 
 {.passC: &"-I{curSrcPath}/" .}
 {.passL: &"{cacheDir}/gemmasimdjsonc.o {cacheDir}/simdjson.o".}
 
-when defined(linux):
+when defined(windows):
+  {.passL: "-lstdc++" .}
+elif defined(linux):
   {.passL: "-lstdc++" .}
 elif defined(macosx):
   {.passL: "-lc++" .}
